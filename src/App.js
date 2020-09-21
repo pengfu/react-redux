9772e3eab2a36707a2de01bf968b66ef7c61336b
chang_su <chang_su@hunliji.com>
2020-09-21 23:35:56 +0800

    show how to use redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import store from '../src/store/ReduxStore'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class ReduxStorePage extends Component {
+  componentDidMount() {
+    store.subscribe(() => {
+      // this.forceUpdate()
+      this.setState({})
+    })
+  }
+  add = () => {
+    store.dispatch({ type: 'add' })
+  }
+  minus = () => {
+    store.dispatch({ type: 'minus' })
+  }
+  stayStatic = () => {
+    store.dispatch({ type: 'others' })
+  }
+  render() {
+    return (
+      <div>
+        <h1>ReduxPage</h1>
+        <p>{store.getState()}</p>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+        <button onClick={this.stayStatic}>static</button>
+      </div>
+    )
+  }
 }
-
-export default App;
